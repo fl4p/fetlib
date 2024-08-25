b8cdb9f66aa6bf3e6b9acd4e8d6869a9a6e3f9c9
Fabian <fabian@schlieper.email>
2024-08-25 21:56:03 +0100

    use new switching loss model

--- dslib/powerloss.py
+++ dslib/powerloss.py
@@ -87,39 +87,43 @@ class SwitchPowerLoss():
         return sum(v for v in self.values() if not math.isnan(v))
 
 
-def dcdc_buck_hs(dc: DcDcSpecs, mf: MosfetSpecs):
-    # https://fscdn.rohm.com/en/products/databook/applinote/ic/power/switching_regulator/power_loss_appli-e.pdf
-    # https://www.richtek.com/Design%20Support/Technical%20Document/AN009#Ripple%20Factor
-    assert mf.tRise is not None and mf.tFall is not None, 'tRise and tFall must be set ' + mf.__repr__()
-    assert mf.Qg is not None, 'Qg must be set ' + mf.__repr__()
-    assert mf.Qrr is not None, 'Qrr must be set ' + mf.__repr__()
-    assert math.isnan(dc.Iripple) or dc.Iripple > 0
-
+def mosfet_switching_trf(dc: DcDcSpecs, mf: MosfetSpecs):
     tr = mf.tRise
     tf = mf.tFall
     if math.isnan(tr) and not math.isnan(tf):
+        warnings.warn('tRise nan, assuming tFall')
         tr = tf
 
-    if math.isnan(tf) and not math.isnan(tr):
+    elif math.isnan(tf) and not math.isnan(tr):
         warnings.warn('tFall nan, assuming 1.5*tRise')
         tf = tr * 1.5
 
     if math.isfinite(dc.Iripple):
-        assert 0 <= dc.Iripple < 2 * dc.Io, 'CCM required, DCM not supported TODO'
-        hrp = dc.Iripple / 2
-        P_sw = 0.5 * dc.Vi * dc.f * (tr * (dc.Io - hrp) + tf * (dc.Io + hrp))
+        assert dc.is_ccm, 'CCM required, DCM not supported TODO'
+        P_sw = 0.5 * dc.Vi * dc.f * (tr * dc.Io_min + tf * dc.Io_max)
     else:
         P_sw = 0.5 * dc.Vi * dc.Io * dc.f * (tr + tf)
+    return P_sw
+
+
+def dcdc_buck_hs(dc: DcDcSpecs, mf: MosfetSpecs, rg_total,fallback_V_pl=math.nan):
+    # https://fscdn.rohm.com/en/products/databook/applinote/ic/power/switching_regulator/power_loss_appli-e.pdf
+    # https://www.richtek.com/Design%20Support/Technical%20Document/AN009#Ripple%20Factor
+
+    assert mf.Qg is not None, 'Qg must be set ' + mf.__repr__()
+    assert mf.Qrr is not None, 'Qrr must be set ' + mf.__repr__()
+    assert math.isnan(dc.Iripple) or dc.Iripple > 0
 
     i_rms2 = dc.D_buck * dc.Io_mean_squared_on
 
     return SwitchPowerLoss(
         P_on=i_rms2 * mf.Rds_on,
-        P_sw=P_sw,
+        # P_sw=mosfet_switching_trf(dc, mf),
+        P_sw=sum(mosfet_switching_hs(dc, mf, rg_total=rg_total, fallback_V_pl=fallback_V_pl)),
         P_rr=0,  # body diode never conducts
-        # P_rr=dc.Vi * dc.f * mf.Qrr, # P_rr is caused by LS but dissipated by HS
         P_gd=dc.Vgs * dc.f * 2 * mf.Qg,
-        P_coss=.5 * dc.f * dc.Vi ** 2 * mf.Coss,
+        # P_coss=.5 * dc.f * dc.Vi ** 2 * mf.Coss, # https://www.onelectrontech.com/power-mosfet-capacitance-coss-and-switching-loss/
+        # P_coss=.5 * dc.f * dc.Vi * mf.Qoss,  #<- https://www.ti.com/lit/an/slpa009a/slpa009a.pdf#page=8
     )
 
 
@@ -134,7 +138,7 @@ def dcdc_buck_ls(dc: DcDcSpecs, mf: MosfetSpecs):
     assert dc.tDead and not math.isnan(dc.tDead), "no dead-time specified %s" % dc.tDead
 
     if not mf.Vsd or math.isnan(mf.Vsd):
-        warnings.warn('no Vsd specified, assuming 1 V')
+        # warnings.warn('no Vsd specified, assuming 1 V')
         vsd = 1
     else:
         vsd = mf.Vsd
@@ -144,12 +148,13 @@ def dcdc_buck_ls(dc: DcDcSpecs, mf: MosfetSpecs):
         P_dt=vsd * dc.Io * (dc.tDead * 2) * dc.f,
         P_rr=dc.Vi * dc.f * mf.Qrr,  # this is dissipated in HS
         P_gd=dc.Vgs * dc.f * 2 * mf.Qg,
-        P_coss=.5 * dc.f * dc.Vi ** 2 * mf.Coss,
+        # P_coss=.5 * dc.f * dc.Vi ** 2 * mf.Coss,
     )
 
 
 class CoilSpecs():
     def __init__(self, Rdc):
+        # TODO skin effect
         self.Rdc = Rdc
 
 
@@ -161,7 +166,7 @@ def dcdc_buck_coil(dc: DcDcSpecs, coil: CoilSpecs):
         * skin effect TODO
     * Core Loss
 
-    Well designed coils have a 50/50% distribution of Wire and Core Loss
+    Well designed coils have a 80/20% distribution of Wire and Core Loss
 
 
     ref https://fscdn.rohm.com/en/products/databook/applinote/ic/power/switching_regulator/buck_converter_efficiency_app-e.pdf
@@ -187,33 +192,48 @@ def dcdc_buck_coil(dc: DcDcSpecs, coil: CoilSpecs):
     )
 
 
-def mosfet_switching_hs(dc: DcDcSpecs, hs: MosfetSpecs):
+def mosfet_switching_hs(dc: DcDcSpecs, hs: MosfetSpecs, rg_total: float, fallback_V_pl=math.nan):
+    """
+    Uses:
+    - V_pl (higher Vpl means more loss during turn off at peak ripple current)
+    - Qsw
+
+    :param dc:
+    :param hs:
+    :param rg_total:
+    :return:
+    """
     # https://www.ti.com/lit/an/slpa009a/slpa009a.pdf#page=3  3.1.1
-    rgTotal = 6
-    ig = (dc.Vgs - hs.V_pl) / (rgTotal)
-    t_sw = hs.Qsw * 1e-9 / ig
-    P_sw = dc.Vi * dc.Io * dc.f * t_sw
-    return P_sw
+    vpl = fallback_V_pl if math.isnan(hs.V_pl) else hs.V_pl
+    ig_on = (dc.Vgs - vpl) / rg_total
+    ig_off = (vpl) / rg_total
+    t_on = hs.Qsw * 1e-9 / ig_on
+    t_off = hs.Qsw * 1e-9 / ig_off
+    tr = max(t_on, hs.tRise)
+    tf = max(t_off, hs.tFall)
+    Psw_on = 0.5 * dc.Vi * dc.f * dc.Io_min * tr
+    Psw_off = 0.5 * dc.Vi * dc.f * dc.Io_max * tf
+    return Psw_on, Psw_off
 
 
-def mosfet_switching_hs_lcsi(dc: DcDcSpecs, hs: MosfetSpecs, ls: MosfetSpecs, rg_total, Lcsi):
+def mosfet_switching_hs_lcsi(dc: DcDcSpecs, hs: MosfetSpecs, ls: MosfetSpecs, rg_total: float, Lcsi: float):
     # loss with L_csi considerations
     # https://www.ti.com/lit/an/slpa009a/slpa009a.pdf
     Qgs2 = hs.Qgs2
 
     # pw on
-    ig1_on = (dc.Vgs - hs.V_pl) / (rg_total + Lcsi * (dc.Io - dc.Iripple / 2) / Qgs2 * 1e9)
+    ig1_on = (dc.Vgs - hs.V_pl) / (rg_total + (Lcsi * dc.Io_min / Qgs2 * 1e9))
     a = (Lcsi * ls.Qoss / hs.Qgd ** 2 * 1e9) if Lcsi else 0
     b = rg_total
     c = -(dc.Vgs - hs.V_pl)
     ig2_on = (-b + math.sqrt(b ** 2 - 4 * a * c)) / (2 * a)
-    Psw_on = 0.5 * dc.Vi * (dc.Io - dc.Iripple / 2) * dc.f * (Qgs2 / ig1_on + hs.Qgd / ig2_on) * 1e-9
+    Psw_on = 0.5 * dc.Vi * dc.Io_min * dc.f * (Qgs2 / ig1_on + hs.Qgd / ig2_on) * 1e-9
 
     # pw off
-    ig1_off = hs.V_pl / (b + Lcsi * (dc.Io + dc.Iripple / 2) / Qgs2 * 1e9)
+    ig1_off = hs.V_pl / (rg_total + Lcsi * dc.Io_max / Qgs2 * 1e9)
     c = - hs.V_pl
     ig2_off = (-b + math.sqrt(b ** 2 - 4 * a * c)) / (2 * a)
-    Psw_off = 0.5 * dc.Vi * (dc.Io + dc.Iripple / 2) * dc.f * (Qgs2 / ig1_off + hs.Qgd / ig2_off) * 1e-9
+    Psw_off = 0.5 * dc.Vi * dc.Io_max * dc.f * (Qgs2 / ig1_off + hs.Qgd / ig2_off) * 1e-9
 
     return Psw_on, Psw_off
 
@@ -246,6 +266,8 @@ def tests_lcsi():
     hs.Qgs = 9.8
     hs.Qoss = 71
     hs.Qgd = 5.4
+    hs._Vpl = 4.2
+    hs._Qgs2 = math.nan
 
     """
         hs.Qg_th = 24
@@ -254,14 +276,41 @@ def tests_lcsi():
     hs.Qgd =17
     """
 
-    P_sw = mosfet_switching_hs(dcdc, hs)
+    Pon, Poff = mosfet_switching_hs(dcdc, hs, rg_total=6)
+    assert abs(Pon) < abs(Poff)
+    assert 0.6 < (Pon / Poff)
 
     ls = hs
-    P_sw_lcsi = sum(mosfet_switching_hs_lcsi(dcdc, hs, ls, rg_total=6e-9, Lcsi=.1e-9))
+    Pon_lcsi0, Poff_lcsi0 = mosfet_switching_hs_lcsi(dcdc, hs, ls, rg_total=6, Lcsi=.01e-9)
+    assert abs(Pon - Pon_lcsi0) / Pon < 0.05
+    assert abs(Poff - Poff_lcsi0) / Poff < 0.05
 
-    print(P_sw_lcsi)
+    Pon_lcsi2, Poff_lcsi2 = mosfet_switching_hs_lcsi(dcdc, hs, ls, rg_total=6, Lcsi=2e-9)
+    assert Pon_lcsi2 < Poff_lcsi2
+    assert Pon_lcsi2 > Pon_lcsi0
+    assert Poff_lcsi2 > Poff_lcsi0
+
+
+def plot_vpl_curve():
+    dcdc = DcDcSpecs(70, 35, 40_000, 10, 500e-9, 33, ripple_factor=0.01)
+    hs = MosfetSpecs.from_mpn('CSD19503KCS', mfr='ti')
+    hs.Qg_th = 6.1
+    hs.Qgs = 9.8
+    hs.Qoss = 71
+    hs.Qgd = 5.4
+    hs._Vpl = 4.2
+    hs._Qgs2 = math.nan
+
+    """
+        hs.Qg_th = 24
+    hs.Qgs = 37
+    hs.Qoss = 335
+    hs.Qgd =17
+    """
+
+    Pon, Poff = mosfet_switching_hs(dcdc, hs, rg_total=6)
 
 
 if __name__ == '__main__':
-    tests()
+    # tests()
     tests_lcsi()
