a2b748775fb3166d6d04735987b4b17b318f35a0
Fabian <fabian@schlieper.email>
2024-09-01 21:36:20 +0100

    add read_llm_json.py

--- read_llm_json.py
+++ read_llm_json.py
@@ -1,20 +1,18 @@
-
-
 import glob
 import json
-import os
 
+from dslib.field import Field
 from dslib.pdf2txt import normalize_dash
 from dslib.pdf2txt.parse import get_field_detect_regex
 
-file_paths = sorted(glob.glob("../fet-datasheets/data/**/*.json", recursive=True))
+file_paths = sorted(glob.glob("../fet-datasheets/intermediate/infineon/**/llm_*.json", recursive=True))
 
 for file_path in file_paths:
     with open(file_path, "r") as f:
         data = json.load(f)
 
-    mfr = file_path.split('/')[-4]
-    mpn = os.path.basename(file_path).split('.')[0]
+    mfr = file_path.split('/')[-3]
+    mpn = file_path.split('/')[-2]
     fields_detect = get_field_detect_regex(mfr)
 
     print(mfr, mpn)
@@ -27,11 +25,16 @@ for file_path in file_paths:
                 continue
             for field_sym, field_re in fields_detect.items():
                 detect_field = lambda s: (len(str(s)) < 80) and field_re.search(normalize_dash(str(s)).strip(' -'))
-                m = next(filter(detect_field, [row_name, row.get('symbol','')]), None)
+                name_candidates = [row_name, row.get('symbol', ''), row.get('parameter', '')]
+                m = next(filter(detect_field, name_candidates), None)
 
                 if m:
+                    # TODO symbol-aware min/typ/max shift to solve mapping errors
+                    try:
+                        Field(field_sym, min=row.get('min'), typ=row.get('typ'), max=row.get('max'))
+                    except Exception as e:
+                        print(mfr, mpn, 'error parsing field', e, row)
                     print(mfr, mpn, field_sym, row)
                     break
 
-
-    #print(file_path, data)
+    # print(file_path, data)
