8932d4ad5b1e4b5b8bb615882029a4ec29c97d91
Fabian <fabian@schlieper.email>
2024-09-08 09:16:23 +0100

    discovery

--- discover_parts.py
+++ discover_parts.py
@@ -1,28 +1,50 @@
+import asyncio
 import os
 from typing import List
 
+from dslib import parts_discovery
 from dslib.fetch import fetch_datasheet
-from dslib.parts_discovery import infineon_mosfets, ti_mosfets, DiscoveredPart
+from dslib.parts_discovery import DiscoveredPart
 from dslib.pdf2txt.parse import parse_datasheet
 
-parts: List[DiscoveredPart] = []
 
-parts += ti_mosfets()
+async def discover_mosfets():
+    parts: List[DiscoveredPart] = []
 
-parts += infineon_mosfets(
-    Vds_min=60,
-    Rds_on_max=20e-3,
-)
+    parts += await parts_discovery.onsemi_mosfets()
+    parts += await parts_discovery.toshiba_mosfets()
+    parts += await parts_discovery.ti_mosfets()
+    parts += await parts_discovery.infineon_mosfets()
 
-parts = [p for p in parts if p.specs.Vds_max >= 60]
+    return parts
 
-download = [p for p in parts if not os.path.exists(p.get_ds_path())]
+def move_low_voltage_datasheets(parts):
+    parts = [p for p in parts if p.specs.Vds_max < 80 or p.specs.Vds_max > 200]
+    for p in parts:
+        if os.path.exists(p.get_ds_path()):
+            print('rename', p.get_ds_path())
+            d = os.path.dirname('lv-' + p.get_ds_path())
+            os.path.exists(d) or os.makedirs(d, exist_ok=True)
+            os.rename(p.get_ds_path(), 'lv-' + p.get_ds_path())
 
-i = 0
-for part in download:
-    i += 1
-    print('download', i, '/', len(download))
-    fetch_datasheet(part.ds_url, part.get_ds_path(), mfr=part.mfr, mpn=part.mpn)
+if __name__ == '__main__':
 
-for part in parts:
-    parse_datasheet(part.get_ds_path())
+    parts = asyncio.run(discover_mosfets())
+
+    move_low_voltage_datasheets(parts)
+    #exit(0)
+
+    parts = [p for p in parts if p.specs.Vds_max >= 80 and p.specs.Vds_max <= 200] # 48V battery
+
+    download = [p for p in parts if not os.path.exists(p.get_ds_path())]
+
+    i = 0
+    for part in download:
+        i += 1
+        print('download', i, '/', len(download))
+        fetch_datasheet(part.ds_url, part.get_ds_path(), mfr=part.mfr, mpn=part.mpn)
+
+    for part in parts:
+        if os.path.exists(part.get_ds_path()):
+            print(part.get_ds_path())
+            parse_datasheet(part.get_ds_path())
--- dslib/parts_discovery.py
+++ dslib/parts_discovery.py
@@ -1,8 +1,6 @@
-import asyncio
 import datetime
 import math
 import os.path
-import time
 from typing import Literal
 
 import pandas as pd
@@ -14,6 +12,10 @@ class MosfetBasicSpecs():
     def __init__(self, Vds_max, Rds_on_10v_max, ID_25, Vgs_th_max, Qg_typ, Qg_max):
         self.Vds_max = Vds_max
         self.Rds_on_10v_max = Rds_on_10v_max
+        self.ID_25 = ID_25
+        self.Vgs_th_max = Vgs_th_max
+        self.Qg_typ = Qg_typ
+        self.Qg_max = Qg_max
 
 
 class DiscoveredPart():
@@ -39,96 +41,63 @@ async def download_parts_list(mfr, url, fn_ext: Literal['csv', 'xlsx'], **kwargs
             **kwargs,
         )
 
+    return fn
 
-async def ti_mosfets_async():
-    parts = []
-    vds_min = -100
-    vds_max = 30
 
+async def ti_mosfets():
     import openpyxl.styles.colors
     import re
     openpyxl.styles.colors.aRGB_REGEX = re.compile("^([A-Fa-f0-9]{8}|[A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$")
 
+    fn = await download_parts_list(
+        'ti',
+        url='https://www.ti.com/power-management/mosfets/products.html',
+        fn_ext='xlsx',
+        click='ti-button.ti-selection-tool-action-bar-download')
+
+    df = pd.read_excel(fn, engine_kwargs=dict(data_only=False)).iloc[9:]
+    df.columns = df.iloc[0]
+    df = df.iloc[1:]
+    df.to_csv(fn.replace('.xlsx', '.csv'), index=False)
+
+    parts = []
     link_reg = re.compile(r'=HYPERLINK\("(?P<url>.+)", "(?P<text>.+)"\)')
 
-    while vds_max < 200:
-        from dslib.fetch import download_with_chromium
-        os.makedirs('parts-lists/ti', exist_ok=True)
-        fn = datetime.datetime.now().strftime(f'parts-lists/ti/mosfet_{vds_min}-{vds_max}V_%Y-%m.xlsx')
-
-        if not os.path.exists(fn):
-            await download_with_chromium(
-                f"https://www.ti.com/power-management/mosfets/products.html?t={time.time()}#267={vds_min}%3B{vds_max}&",
-                filename=fn,
-                click='ti-button.ti-selection-tool-action-bar-download',
-                close='page',
+    for i, row in df.iterrows():
+        parts.append(DiscoveredPart(
+            mfr='ti',
+            mpn=link_reg.match(row['Product or Part number']).groupdict().get('text'),
+            # mpn2=row['Product'],
+            ds_url=link_reg.match(row['PDF data sheet']).groupdict().get('url'),
+            specs=MosfetBasicSpecs(
+                Vds_max=row['VDS (V)'],
+                Rds_on_10v_max=row['Rds(on) at VGS=10 V (max) (mΩ)'],
+                ID_25=row['ID - continuous drain current at TA=25°C (A)'],
+                Vgs_th_max=row['VGS (V)'],
+                Qg_typ=math.nan,
+                Qg_max=math.nan,
             )
-
-        df = pd.read_excel(fn, engine_kwargs=dict(data_only=False)).iloc[9:]
-        df.columns = df.iloc[0]
-        df = df.iloc[1:]
-        if df['VDS (V)'].min() < vds_min and len(df) >= 200:
-            break
-        assert len(df) < 200
-        assert df['VDS (V)'].min() >= vds_min
-        assert df['VDS (V)'].max() <= vds_max
-
-        df.to_csv(fn.replace('.xlsx', '.csv'), index=False)
-
-        for i, row in df.iterrows():
-            parts.append(DiscoveredPart(
-                mfr='ti',
-                mpn=link_reg.match(row['Product or Part number']).groupdict().get('text'),
-                # mpn2=row['Product'],
-                ds_url=link_reg.match(row['PDF data sheet']).groupdict().get('url'),
-                specs=MosfetBasicSpecs(
-                    Vds_max=row['VDS (V)'],
-                    Rds_on_10v_max=row['Rds(on) at VGS=10 V (max) (mΩ)'],
-                    ID_25=row['ID - continuous drain current at TA=25°C (A)'],
-                    Vgs_th_max=row['VGS (V)'],
-                    Qg_typ=math.nan,
-                    Qg_max=math.nan,
-                )
-            ))
-
-        vds_min = vds_max + 1
-        vds_max = vds_min + 40
-
+        ))
     return parts
 
 
-def ti_mosfets():
-    return asyncio.run(ti_mosfets_async())
+async def infineon_mosfets():
+    fn = await download_parts_list(
+        'infineon',
+        'https://www.infineon.com/cms/en/design-support/finder-selection-tools/product-finder/mosfet-finder/',
+        fn_ext='xlsx',
+        click='a[data-track-name="downloadLink"]',
+        close=True,
+    )
 
-
-def infineon_mosfets(Vds_min, Rds_on_max):
-    import pandas as pd
-
-    os.makedirs('parts-lists/infineon', exist_ok=True)
-    fn = datetime.datetime.now().strftime('parts-lists/infineon/mosfet-%Y-%m.xlsx')
-
-    if not os.path.isfile(fn):
-        from dslib.fetch import download_with_chromium
-        f = download_with_chromium(
-            'https://www.infineon.com/cms/en/design-support/finder-selection-tools/product-finder/mosfet-finder/',
-            filename=fn,
-            click='a[data-track-name="downloadLink"]',
-            close=True,
-        )
-        asyncio.run(f)
-
-        df = pd.read_excel(fn)
-        df.to_csv(fn.replace('.xlsx', '.csv'), index=False)
-    else:
-        df = pd.read_excel(fn)
+    df = pd.read_excel(fn)
+    df.to_csv(fn.replace('.xlsx', '.csv'), index=False)
 
     parts = []
     for i, row in df.iterrows():
         vds = row['VDS max [V]']
         if isinstance(vds, str):
             vds = float(vds.split('_')[0])
-        if Vds_min and abs(vds) < Vds_min:
-            continue
         parts.append(DiscoveredPart(
             mfr='infineon',
             mpn=row['OPN'],
@@ -148,44 +117,65 @@ def infineon_mosfets(Vds_min, Rds_on_max):
 
 
 async def toshiba_mosfets():
-    #
-    # document.querySelector('button.funcBtn_item.funcBtn_item--last.funcBtn_item-download').click()
-    # document.querySelector('ul.funcBtn_item-download_accordion.funcBtn_item-download_accordion-file li:last-child button').click()
-
-    os.makedirs('parts-lists/infineon', exist_ok=True)
-    fn = datetime.datetime.now().strftime('parts-lists/toshiba/mosfet-%Y-%m.xlsx')
-
-    download_with_chromium(
+    fn = await download_parts_list(
+        'toshiba',
         "https://toshiba.semicon-storage.com/parametric/product?code=param_304",
-        fn,
+        fn_ext='xlsx',
         click=[
             'button.funcBtn_item.funcBtn_item--last.funcBtn_item-download',
-            'ul.funcBtn_item-download_accordion.funcBtn_item-download_accordion-file li:last-child button',
+            # 'ul.funcBtn_item-download_accordion.funcBtn_item-download_accordion-file li:last-child button', # csv
+            'ul.funcBtn_item-download_accordion.funcBtn_item-download_accordion-file li:first-child button',  # xlsx
         ]
     )
 
-    return
+    import openpyxl
+
+    wb = openpyxl.load_workbook(fn)
+    ws = wb['param_304_en']
+
+    cols = {}
+    cl = list(ws.columns)
+    for i in range(len(cl)):
+        n = ws.cell(row=1, column=i + 1).value
+        cols[n] = list(cl[i])
+
+    parts = []
+    for i in range(1, len(list(ws.rows))):
+        parts.append(DiscoveredPart(
+            mfr='toshiba',
+            mpn=cols['Part Number'][i].value,
+            # mpn2=row['Product'],
+            ds_url=cols['Datasheet'][i].hyperlink and cols['Datasheet'][i].hyperlink.target,
+            specs=MosfetBasicSpecs(
+                Vds_max=float(cols['VDSS(V)'][i].value or 'nan'),
+                Rds_on_10v_max=float(cols['RDS(ON)Max(Ω)|VGS|=10V'][i].value or 'nan'),
+                ID_25=float(cols['RDS(ON)Max(Ω)|VGS|=10V'][i].value or 'nan'),
+                Vgs_th_max=math.nan,
+                Qg_typ=float(cols['RDS(ON)Max(Ω)|VGS|=10V'][i].value or 'nan'),
+                Qg_max=math.nan,
+            )
+        ))
+
+    return parts
 
 
-def onsemi_mosfets():
+async def onsemi_mosfets():
     # 200V:
     #
 
-    os.makedirs('parts-lists/onsemi', exist_ok=True)
-    fn = datetime.datetime.now().strftime('parts-lists/onsemi/mosfet-%Y-%m.xlsx')
+    fn = await download_parts_list(
+        'onsemi',
+        url='https://www.onsemi.com/products/discrete-power-modules/mosfets/low-medium-voltage-mosfets',
+        fn_ext='csv',
+        click='button.btn-export',
+    )
 
-    if not os.path.isfile(fn):
-        from dslib.fetch import download_with_chromium
-        f = download_with_chromium(
-            'https://www.onsemi.com/products/discrete-power-modules/mosfets/low-medium-voltage-mosfets',
-            filename=fn,
-            click='button.btn-export',
-            close=True,
-        )
+    df = pd.read_csv(fn)
+
+    return []
+
 
     # https://www.onsemi.com/download/data-sheet/pdf/ech8667-d.pdf
-
-    # document.querySelector('button.btn-export').click()
 
 
 if __name__ == '__main__':
