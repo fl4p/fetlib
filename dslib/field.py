55b3c5dbdfa5353f1c98b69d6a7729755a2b3f24
Fabian <fabian@schlieper.email>
2024-09-22 16:41:35 +0100

    add DatasheetFields

--- dslib/field.py
+++ dslib/field.py
@@ -107,3 +107,160 @@ def parse_field_value(s):
     if not s or s == '-' or set(s) == {'-'}:
         return math.nan
     return float(s)
+
+
+class MpnMfr:
+    def __init__(self, mfr, mpn):
+        self.mfr = mfr
+        self.mpn = mpn
+
+
+class DatasheetFields():
+    def __init__(self, mfr=None, mpn=None, fields: Iterable[Field] = None):
+        self.part = MpnMfr(mpn, mfr)
+        self.fields_filled: Dict[str, Field] = {}
+        self.fields_lists: Dict[str, List[Field]] = {}
+        if fields:
+            self.add_multiple(fields)
+
+    def add(self, f: Field):
+        assert not math.isnan(f.typ_or_max_or_min)
+        if f.symbol not in self.fields_filled:
+            self.fields_filled[f.symbol] = copy(f)
+            self.fields_lists[f.symbol] = []
+        self.fields_lists[f.symbol].append(f)
+        self.fields_filled[f.symbol].fill(f)
+
+    def add_multiple(self, fields: Iterable[Field]):
+        for f in fields:
+            self.add(f)
+
+    def get_mosfet_specs(self):
+        mf_fields = [
+            'Qrr', 'Vsd',  # body diode
+            'Qgd', 'Qgs', 'Qgs2', 'Qg_th',  # gate charges
+            'Coss', 'Qsw',
+        ]
+        field_mul = lambda sym: 1 if sym[0] == 'V' else 1e-9
+
+        ds = self
+
+        from dslib.spec_models import MosfetSpecs
+        return MosfetSpecs(
+            Vds_max=ds.get_max('Vds', True),
+            Rds_on=ds.get_max('Rds_on_10v'),
+            Qg=ds.get_typ_or_max_or_min('Qg') * 1e-9,
+            tRise=ds.get_typ_or_max_or_min('tRise') * 1e-9,
+            tFall=ds.get_typ_or_max_or_min('tFall') * 1e-9,
+            **{k: ds.get_typ_or_max_or_min(k) * field_mul(k) for k in mf_fields},
+            Vpl=ds.get_typ_or_max_or_min('Vpl'),
+        )
+
+    def get(self, sym, stat: Union[Tuple[Field.StatLiteral], Field.StatLiteral], required=False):
+        if isinstance(stat, str):
+            stat = (stat,)
+        r = self.fields_filled.get(sym)
+        assert not required or r
+        if not r:
+            return math.nan
+        for s in stat:
+            v = getattr(r, s)
+            if not math.isnan(v):
+                return v
+        return math.nan
+
+    def get_typ_or_max_or_min(self, sym, required=False):
+        r = self.fields_filled.get(sym)
+        assert r or not required
+        return math.nan if not r else r.typ_or_max_or_min
+
+    def get_typ(self, sym):
+        r = self.fields_filled.get(sym)
+        return math.nan if not r else r.typ
+
+    def get_max(self, sym, required=False):
+        r = self.fields_filled.get(sym)
+        assert not required or r
+        return math.nan if not r else r.max
+
+    def items(self):
+        return self.fields_filled.items()
+
+    def keys(self):
+        return self.fields_filled.keys()
+
+    def __contains__(self, item):
+        return item in self.fields_filled
+
+    def __len__(self):
+        return len(self.fields_filled)
+
+    def all_fields(self):
+        return sum(map(list, self.fields_lists.values()), [])
+
+    # def _apply_on_values(self, symbols=None, reduce_field):
+    #    if not symbols:
+    #        symbols = b.fields_filled.keys()
+    #    for sym in symbols:
+    #        for f in b.fields_lists.get(sym, []):
+    #
+    #            (reduce_field(sym, stat, f[stat]) ):
+    #                rv = f[stat]
+
+    def count_equal(self, a: 'DatasheetFields', symbols=None, err_threshold=0.05):
+        b = self
+        n = 0
+        if not symbols:
+            symbols = b.fields_filled.keys()
+        for sym in symbols:
+            for stat in Field.Stats:
+                min_err = float('inf')
+                for f in b.fields_lists.get(sym, []):
+                    rv = f[stat]
+                    are = abs((a.get(sym, stat) - rv) / rv)
+                    if are < min_err:
+                        min_err = are
+
+                if min_err < err_threshold:
+                    n += 1
+
+        return n
+
+    def show_diff(self, a: 'DatasheetFields', symbols=None, err_threshold=0.05, title=''):
+        b = self
+        n = 0
+        if not symbols:
+            symbols = b.fields_filled.keys()
+        for sym in symbols:
+            min_err = float('inf')
+            for f in b.fields_lists.get(sym, []):
+                max_err = 0
+                for stat in cast(List[Field.StatLiteral], ['min', 'typ', 'max']):
+                    # TODO iterate fields_list and take min err
+                    rv = f[stat]
+                    are = abs((a.get(sym, stat) - rv) / rv)
+                    max_err = max(max_err, are)
+                if max_err >= err_threshold:
+                    print('')
+                    print(title, self.part.mfr, self.part.mpn,
+                          f'err {round(max_err, 3)} > {err_threshold}',
+                          '\nref=', f,
+                          '\noth=', a.fields_filled[sym]
+                          )
+                    n += 1
+
+                if max_err < min_err:
+                    min_err = max_err
+
+        return n
+
+    def rmse(self, b: 'DatasheetFields'):
+        raise NotImplemented()
+
+        ref = self
+
+        rmse = {}
+        for sym, fl in ref.fields_filled:
+            if sym not in b:
+                rmse[sym] = math.nan
+            rmse[sym] = min(() / r[s] for f in fl)
