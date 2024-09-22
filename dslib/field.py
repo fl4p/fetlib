f810cae5cfa3af72c3b70aeda13365d7b0035893
Fabian <fabian@schlieper.email>
2024-09-22 16:40:24 +0100

    Field methods

--- dslib/field.py
+++ dslib/field.py
@@ -1,9 +1,17 @@
 import math
+from copy import copy
+from typing import List, Iterable, Dict, Literal, Tuple, Union, cast
 
 from dslib.pdf2txt import normalize_dash
 
 
+def first(a):
+    return next((x for x in a if x and not math.isnan(x)), math.nan)
+
+
 class Field():
+    StatLiteral = Literal['min', 'max', 'typ']
+    Stats = cast(List[StatLiteral], ['min', 'typ', 'max'])
 
     def __init__(self, symbol: str, min, typ, max, mul=1, cond=None, unit=None):
         self.symbol = symbol
@@ -53,6 +61,9 @@ class Field():
     def __repr__(self):
         return f'Field("{self.symbol}", min={self.min}, typ={self.typ}, max={self.max}, unit="{self.unit}", cond={repr(self.cond)})'
 
+    def __str__(self):
+        return f'{self.symbol} = %5.1f,%5.1f,%5.1f [%s] (%s)' % (self.min, self.typ, self.max, self.unit, self.cond)
+
     @property
     def typ_or_max_or_min(self):
         if not math.isnan(self.typ):
@@ -72,6 +83,19 @@ class Field():
         elif not math.isnan(self.min):
             return self.min
         raise ValueError()
+
+    def fill(self, f: 'Field', update_min_max=False):
+        if update_min_max:
+            raise NotImplemented()
+            # TODO min, max updates?
+
+        for s in ('min', 'max', 'typ'):
+            if math.isnan(getattr(self, s)) and not math.isnan(getattr(f, s)):
+                setattr(self, s, getattr(f, s))
+
+    def __getitem__(self, item):
+        assert item in {'min', 'max', 'typ'}
+        return getattr(self, item)
 
 
 def parse_field_value(s):
