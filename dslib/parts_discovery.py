9f04b6a5b1e975e983ccb9270630106a0cfd5429
Fabian <fabian@schlieper.email>
2024-12-28 15:29:41 +0000

    huayi_mosfets

--- dslib/parts_discovery.py
+++ dslib/parts_discovery.py
@@ -429,6 +429,52 @@ async def nexperia_mosfets():
     return
 
 
+@disk_cache(ttl='7d', hash_func_code=True)
+def huayi_mosfets():
+    from bs4 import BeautifulSoup
+    parts = []
+    page = 1
+    while True:
+        u = (f"http://en.hymexa.com/index.php?s=api&c=api&m=template&name=search_data.html&module=products"
+             f"&catid=40&searchid=6932af211e291898af63e5d04ae75735&sototal=9999&order=&params=&format=json&page={page}")
+        resp = requests.get(u).json()
+        assert resp['code'] == 1
+        ht = resp['msg']
+        if not ht:
+            break
+        for row in BeautifulSoup(ht, 'html.parser').find_all('div', attrs={'class': 'tr_box_wai'}):
+            mpn = row.find_next('div', attrs={'class': 'vg_title'}).text
+            # vg_rdson_max_10vm
+            # vg_ciss_typpf
+            # vg_package
+            vds = float(row.find_next('div', attrs={'class': 'vg_vds_min_v'}).text)
+            rds = parse_field_value(row.find_next('div', attrs={'class': 'vg_rdson_max_10vm'}).text) * 1e-3
+            qg = float(row.find_next('div', attrs={'class': 'vg_qg_typnc'}).text)
+            if rds < 20e-3 and vds > 200:
+                rds *= 1e3
+            if qg < 1 and rds < 0.01:
+                qg = math.nan
+
+            m = re.search(r"<a href=\"/generic/web/viewer\.html\?file=\.\./\.\./(.+?\.pdf)", str(row))
+            if not m:
+                print(mpn, 'no DS url', whitespaces_to_space(str(row)))
+            ds_url = ('http://en.hymexa.com/' + m[1]) if m else None
+            parts.append(DiscoveredPart('huayi', mpn, ds_url=ds_url, specs=MosfetBasicSpecs(
+                Vds_max=vds,
+                Rds_on_10v_max=rds,
+                Qg_max=math.nan,
+                Qg_typ=qg,
+                ID_25=float(row.find_next('div', attrs={'class': 'vg_idtc'}).text),
+                Vgs_th_min=math.nan,
+                Vgs_th_typ=math.nan,
+                Vgs_th_max=math.nan,
+                source='en.hymexa.com'
+            ), package=row.find_next('div', attrs={'class': 'vg_package'}).text.strip()))
+        page += 1
+
+    return parts
+
+
 async def st_mosfets():
     df = pd.read_excel('parts-lists/st/stpower-nch-mosfet-30v-200v-to220.xlsx')
     hi = df.iloc[:, 0].str.startswith('Part').idxmax()
@@ -587,6 +633,8 @@ def benchmark_mpns():
 
 if __name__ == '__main__':
     async def _main():
+        await huayi_mosfets()
+
         await vishay_mosfets()
 
         u = onsemi_ds_url("NTDV20N06T4G")
